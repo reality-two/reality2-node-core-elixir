e4d71439d781280a173d52e77b00b1f18d57ad53
Roy Davies <roy@reality2.ai>
2024-06-15 06:03:04 +1200

    Start on disc backup plugin

---
+++ apps/ai_reality2_backup/lib/ai_reality2_backup.ex
@@ -0,0 +1,18 @@
+defmodule AiReality2Backup do
+  @moduledoc """
+  Documentation for `AiReality2Backup`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AiReality2Backup.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ apps/ai_reality2_backup/test/ai_reality2_backup_test.exs
@@ -0,0 +1,8 @@
+defmodule AiReality2BackupTest do
+  use ExUnit.Case
+  doctest AiReality2Backup
+
+  test "greets the world" do
+    assert AiReality2Backup.hello() == :world
+  end
+end
