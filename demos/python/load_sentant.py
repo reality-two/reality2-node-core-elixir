53b2ad37cf73193602f756d7425598a3b0114147
Roy C. Davies <roy@reality2.ai>
2024-06-27 06:42:15 +1200

    Python demos updates

--- demos/python/load_sentant.py
+++ demos/python/load_sentant.py
@@ -5,7 +5,14 @@ from reality2 import Reality2 as R2
 import sys
 import time
 import json
+import toml
 from os.path import exists
+from getkey import getkey
+import copy
+import ruamel.yaml
+
+yaml = ruamel.yaml.YAML(typ='safe')
+
 # ----------------------------------------------------------------------------------------------------
 
 
@@ -17,11 +24,11 @@ def printhelp(events):
     print("---------- Send Events ----------")
     counter = 0
     for event in events:
-        print(" Press", counter, "followed by the enter key for [", event["event"], event["parameters"], "]")
+        print(" Press [", counter, "] for {", event["event"], event["parameters"], "}")
         counter = counter + 1
 
-    print(" Press h, followed by the enter key for this help.")
-    print(" Press q, followed by the enter key to quit.")
+    print(" Press [ h ] for help.")
+    print(" Press [ q ] to quit.")
     print("---------------------------------")    
 # ----------------------------------------------------------------------------------------------------
 
@@ -46,18 +53,24 @@ def printout(data):
 # ----------------------------------------------------------------------------------------------------
 def main(filename, host):
 
+    # ------------------------------------------------------------------------------------------------
     # Connect to the Reality2 node
+    # ------------------------------------------------------------------------------------------------
     r2_node = R2(host, 4001)
 
+    # ------------------------------------------------------------------------------------------------
     # Read the definition file
+    # ------------------------------------------------------------------------------------------------
     if (exists(filename)):
         with open(filename, 'r') as file:
             definition = file.read()
     else:
         print("The file", filename, "does not exist.")
         return
-
+    
+    # ------------------------------------------------------------------------------------------------
     # Read the variables file (if it exists)
+    # ------------------------------------------------------------------------------------------------
     if exists('../../../variables.json'):
         with open('../../../variables.json', 'r') as file:
             variables_txt = file.read()
@@ -69,59 +82,85 @@ def main(filename, host):
         for key in variables:
             definition = definition.replace(key, variables[key])
 
-    # Unload any existing Sentants named in the file
-    sentant_name = R2.JSONPath(definition, "sentant.name")
+    # ------------------------------------------------------------------------------------------------
+    # Unload the existing Sentant named in the fileif it exists
+    # ------------------------------------------------------------------------------------------------
+    if filename.endswith('.yaml'):
+        definition_json = yaml.load(definition)
+    elif filename.endswith('.toml'):
+        definition_json = toml.loads(definition)
+    elif filename.endswith('.json'):
+        definition_json = json.loads(definition)
+    else:
+        print("The file", filename, "is not a valid format.")
+        return
+        
+    sentant_name = R2.JSONPath(definition_json, "sentant.name")
+    print("Unloading existing Sentant named \"", sentant_name, "\"")
     r2_node.sentantUnloadByName(sentant_name)
 
+    # ------------------------------------------------------------------------------------------------
     # Load the Sentant
+    # ------------------------------------------------------------------------------------------------
     result = r2_node.sentantLoad(definition, {}, "id name signals events { event parameters}")
 
-    # Grab the ID of the Sentant
+    # ------------------------------------------------------------------------------------------------
+    # Get the ID of the loaded Sentant
+    # ------------------------------------------------------------------------------------------------
     id = R2.JSONPath(result, "sentantLoad.id")
 
+    # ------------------------------------------------------------------------------------------------
     # Get the signals and events
+    # ------------------------------------------------------------------------------------------------
     signals = R2.JSONPath(result, "sentantLoad.signals")
     events = R2.JSONPath(result, "sentantLoad.events")
 
+    # ------------------------------------------------------------------------------------------------
     # Start the subscriptions to the Sentant
+    # ------------------------------------------------------------------------------------------------
     r2_node.awaitSignal(id, "debug", printout)
     for signal in signals:
         r2_node.awaitSignal(id, signal, printout)
 
-    # Wait 1 second to allow the subscriptions to catch up
+    # ------------------------------------------------------------------------------------------------
+    # Wait a second to allow the subscriptions to catch up
+    # ------------------------------------------------------------------------------------------------
     time.sleep(1)
 
+    # ------------------------------------------------------------------------------------------------
     # Print out the help
+    # ------------------------------------------------------------------------------------------------
     printhelp(events)
 
+    # ------------------------------------------------------------------------------------------------
     # Wait for user input and send the events
+    # ------------------------------------------------------------------------------------------------
     while (True):
-        input_str = input()
-        if (input_str == "q"):
+        key = getkey()
+        if (key =="q"):
             break
-        elif (input_str == "h"):
+        elif (key == "h"):
             printhelp(events)
         else:
-            if input_str.isdigit():
-                index = int(input_str)
+            if key.isdigit():
+                index = int(key)
                 if (index >= 0 and index < len(events)):
-                    event = events[int(input_str)]
-                    parameters = event["parameters"]
+                    parameters = copy.deepcopy(events[index]["parameters"])
 
-                    parameter_index = 0
                     for parameter in parameters:
-                        print("Type in a", parameters[parameter], "for", parameter, end=" :")
-                        parameter_str = input()
-                        parameters[parameter] = parameter_str
-    
-                    print("Sending event [", event["event"], "]")
-                    r2_node.sentantSend(id, event["event"], event["parameters"])
+                        print("Type in a", parameters[parameter], "for", parameter, end=" : ")
+                        parameters[parameter] = input()
+
+                    print("Sending event [", events[index]["event"], "]")
+                    r2_node.sentantSend(id, events[index]["event"], parameters)
                 else:
                     print("Please enter a number between 0 and", len(events) - 1, "inclusive.")
             else:
-                print("Please enter a number or q to quit.")
+                print("Please enter a number, h for help, or q to quit.")
 
+    # ------------------------------------------------------------------------------------------------
     # Close the subscriptions
+    # ------------------------------------------------------------------------------------------------
     print("Closing the subscriptions and quitting.")
     r2_node.close()
 # ----------------------------------------------------------------------------------------------------
