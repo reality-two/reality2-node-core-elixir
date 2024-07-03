f8ef294b26a146b5bc7b19060789fb14a20bf06e
Roy Davies <roy@reality2.ai>
2024-07-03 17:26:22 +1200

    Demos tweaks

---
+++ demos/rust/load_sentant/target/debug/deps/load_sentant-0815d5a250062d9a.d
@@ -0,0 +1,5 @@
+/Volumes/Storage/Development/Reality2/reality2-node-core-elixir/demos/rust/load_sentant/target/debug/deps/load_sentant-0815d5a250062d9a: src/main.rs
+
+/Volumes/Storage/Development/Reality2/reality2-node-core-elixir/demos/rust/load_sentant/target/debug/deps/load_sentant-0815d5a250062d9a.d: src/main.rs
+
+src/main.rs:
---
+++ demos/rust/load_sentant/target/debug/load_sentant.d
@@ -0,0 +1,1 @@
+/Volumes/Storage/Development/Reality2/reality2-node-core-elixir/demos/rust/load_sentant/target/debug/load_sentant: /Volumes/Storage/Development/Reality2/reality2-node-core-elixir/demos/rust/load_sentant/src/main.rs
