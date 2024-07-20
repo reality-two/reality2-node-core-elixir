da49ca9a6b17f3812f4979cf268a39eece18ef7e
Roy Davies <roy.c.davies@ieee.org>
2024-07-20 18:57:40 +1200

    WIP Rust Plugin Demo

--- apps/ai_reality2_rustdemo/lib/ai_reality2_rustdemo/application.ex
+++ apps/ai_reality2_rustdemo/lib/ai_reality2_rustdemo/application.ex
@@ -1,20 +1,15 @@
 defmodule AiReality2Rustdemo.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
 
   use Application
 
+
   @impl true
   def start(_type, _args) do
     children = [
-      # Starts a worker by calling: AiReality2Rustdemo.Worker.start_link(arg)
-      # {AiReality2Rustdemo.Worker, arg}
       %{id: AiReality2Rustdemo.Main, start: {AiReality2Rustdemo.Main, :start_link, [AiReality2Rustdemo.Main]}}
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: AiReality2Rustdemo.Supervisor]
     Supervisor.start_link(children, opts)
   end
