8c7ae3eb67ea24cf00cf417f2cd7eb2a4f28a37c
Roy C. Davies <roy.davies@auckland.ac.nz>
2025-02-17 08:38:58 +1300

    Sentant App Tweaks

--- web/sentants/src/lib/SentantCard.svelte
+++ web/sentants/src/lib/SentantCard.svelte
@@ -27,6 +27,9 @@
     let input_text: input_text_type = {};
     let max_messages = 5;
 
+    let show_message = false;
+    let current_message = "";
+
     async function copyMessage(data: string) {
         try {
         await navigator.clipboard.writeText(data);
@@ -107,55 +110,72 @@
 <!----------------------------------------------------------------------------------------------------->
 
 {#if sentant.name != "monitor"}
-    <Card style="height: {height};" id={sentant.id}>
-        <Link ui image href={"/?name=" + sentant.name + "&variables=" + encodeURIComponent(JSON.stringify(variables))}>
-            <Image ui large src="/images/bee_blue.png" />
-        </Link>
-        <Content style={mini?"text-align: center;":"height:150px; text-align: center;"}>
-            <Header>{sentant.name}</Header>
-            <p><Text ui small blue>{sentant.id}</Text></p>
-            <p>{sentant.description}</p>
-        </Content>
-        <Divider ui small horizontal center aligned header blue>messages</Divider>
-        <Content extra style={mini?"text-align: center;":"height:200px; text-align: center;"}>
-            {#each messages as message, i}
-                {#if message.split('|')[0] != ""}
-                    <Text ui popup={{ hoverable: true }}
-                        _={(i == messages.length-1 ? "teal" : "grey")}>
- 
-                        {message.split('|')[0].split(", ")[1]}
-                    </Text>
-                    <Popup ui wide style="">
-                        <Text ui>{JSON.stringify(try_convert(message.split('|')[1]), null, 1)}</Text>
-                    </Popup>
-                    <Button ui icon small basic popup style="box-shadow: 0 0 0 0;" data-tooltip={"copy to clipboard"} on:click={() => copyMessage(JSON.stringify(try_convert(message.split('|')[1]), null, 4))}><Icon ui clipboard/></Button>
-                    <br/>
-                {/if}
-            {/each}
-        </Content>
-        {#if !mini}
-            {#if sentant.events.length > 0}
-                <Divider ui small horizontal center aligned header blue>actions</Divider>
-                <Content extra style="overflow-y:scroll; overflow-x: hidden; height:240px; text-align: center;">
-                    {#each sentant.events as event}
-                        {#each Object.keys(event.parameters) as key}
-                            <Input ui labeled fluid style={"margin-bottom:5px;"}>    
-                                <Input text large placeholder={key} bind:value={input_text[event.event+key]}/>
-                            </Input>
-                        {/each}
-                        <Button ui teal fluid on:click={() => event_button_pressed(sentant.id, event.event)} style={"margin-bottom:20px;"}>
-                            {event.event}
+    {#if show_message}
+        <Card style="height: {height};" id={sentant.id}>
+            <Content style={mini?"text-align: center;":"height:140px; text-align: center;"}>
+                <Header>{current_message.split('|')[0].split(", ")[1]}</Header>
+            </Content>
+            <Content style="overflow:auto;">
+                <Text ui>
+                    <pre style="text-align: left; white-space: pre-wrap; word-wrap: break-word; overflow-wrap: break-word; width: 80ch;">{JSON.stringify(try_convert(current_message.split('|')[1]), null, 4)}</pre>
+                </Text>
+            </Content>
+            <Divider ui small horizontal center aligned header blue>actions</Divider>
+            <Content extra style="height:80px;">
+                <Button ui teal fluid on:click={() => show_message = false}>done</Button>
+            </Content>
+        </Card>
+    {:else}
+        <Card style="height: {height};" id={sentant.id}>
+            <Link ui image href={"/?name=" + sentant.name + "&variables=" + encodeURIComponent(JSON.stringify(variables))}>
+                <Image ui large src="/images/bee_blue.png" />
+            </Link>
+            <Content style={mini?"text-align: center;":"height:140px; text-align: center;"}>
+                <Header>{sentant.name}</Header>
+                <p><Text ui small blue>{sentant.id}</Text></p>
+                <p>{sentant.description}</p>
+            </Content>
+            <Divider ui small horizontal center aligned header blue>messages</Divider>
+            <Content extra style={mini?"text-align: center;":"height:200px; text-align: center;"}>
+                {#each messages as message, i}
+                    {#if message.split('|')[0] != ""}
+                        <Button ui icon small basic popup style="box-shadow: 0 0 0 0;" data-tooltip={"see message"} on:click={() => {
+                                current_message = message;
+                                show_message = true;
+                            }}>
+                            <Text ui _={(i == messages.length-1 ? "teal" : "grey")}>      
+                                {message.split('|')[0].split(", ")[1]}
+                            </Text>                        
                         </Button>
-                        <Divider ui teal></Divider>
-                    {/each}
+                        <Button ui icon small basic popup style="box-shadow: 0 0 0 0;" data-tooltip={"copy to clipboard"} on:click={() => copyMessage(JSON.stringify(try_convert(message.split('|')[1]), null, 4))}><Icon ui clipboard/></Button>
+                        <br/>
+                    {/if}
+                {/each}
+            </Content>
+            {#if !mini}
+                {#if sentant.events.length > 0}
+                    <Divider ui small horizontal center aligned header blue>actions</Divider>
+                    <Content extra style="overflow-y:scroll; overflow-x: hidden; height:240px; text-align: center;">
+                        {#each sentant.events as event}
+                            {#each Object.keys(event.parameters) as key}
+                                <Input ui labeled fluid style={"margin-bottom:5px;"}>    
+                                    <Input text large placeholder={key} bind:value={input_text[event.event+key]}/>
+                                </Input>
+                            {/each}
+                            <Button ui teal fluid on:click={() => event_button_pressed(sentant.id, event.event)} style={"margin-bottom:20px;"}>
+                                {event.event}
+                            </Button>
+                            <Divider ui teal></Divider>
+                        {/each}
+                    </Content>
+                {/if}
+                <Divider ui small horizontal center aligned header orange>danger zone</Divider>
+                <Content extra style="height:80px;">
+                    <Button ui orange fluid basic on:click={unload_sentant}>
+                        Unload
+                    </Button>
                 </Content>
             {/if}
-            <Divider ui small horizontal center aligned header orange>danger zone</Divider>
-            <Content extra style="height:80px;">
-                <Button ui orange fluid basic on:click={unload_sentant}>
-                    Unload
-                </Button>
-            </Content>
-        {/if}
-    </Card>
+        </Card>
+    {/if}
 {/if}
