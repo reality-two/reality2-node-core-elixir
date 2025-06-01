3f594e7c6f0f3fed7abdfff8364fa9341eb3ed39
Roy C. Davies <roy.davies@auckland.ac.nz>
2025-06-02 11:04:56 +1200

    WIP on version control, transient networks and autostart

---
+++ apps/ai_reality2_versioncontrol/lib/ai_reality2_versioncontrol.ex
@@ -0,0 +1,18 @@
+defmodule AiReality2Versioncontrol do
+  @moduledoc """
+  Documentation for `AiReality2Versioncontrol`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AiReality2Versioncontrol.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
